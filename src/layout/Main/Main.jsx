482c4a3620d388c37c521356dc36aae59b2831b3
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-27 23:37:34 +0600

    feat: Navigation component added

---
+++ src/layout/Main/Main.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../../shared/Navigation/Navigation";
+
+const Main = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+        </div>
+    );
+};
+
+export default Main;
