09c4e429dcb6483807c595283b015a2220c35fe1
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-28 23:27:22 +0600

    feat: Service Banner component added

---
+++ src/pages/Service/Service/Service.jsx
@@ -0,0 +1,12 @@
+import Banner from "../Banner/Banner";
+
+
+const Service = () => {
+    return (
+        <div>
+            <Banner></Banner>
+        </div>
+    );
+};
+
+export default Service;
