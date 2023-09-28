59d20cbd61ff07345b15603bfc79606d3f54ff98
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-28 23:57:05 +0600

    feat: Download component added

--- src/pages/Service/Service/Service.jsx
+++ src/pages/Service/Service/Service.jsx
@@ -1,10 +1,19 @@
 import Banner from "../Banner/Banner";
+import Download from "../Download/Download";
 
 
 const Service = () => {
     return (
         <div>
             <Banner></Banner>
+            <div className="flex w-9/12 mx-auto">
+                <div className="w-8/12">
+                    <h2>Hello</h2>
+                </div>
+                <div className="w-4/12">
+                    <Download></Download>
+                </div>
+            </div>
         </div>
     );
 };
