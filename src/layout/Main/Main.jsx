b5534a8bad5b8ab767a62b2fbd7a48f24ce1258b
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-28 12:07:35 +0600

    feat: Banner component added

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 import Navigation from "../../shared/Navigation/Navigation";
 
 const Main = () => {
     return (
         <div>
             <Navigation></Navigation>
+            <Outlet></Outlet>
         </div>
     );
 };
--- src/shared/Navigation/Navigation.jsx
+++ src/shared/Navigation/Navigation.jsx
@@ -3,6 +3,7 @@ import logo from '../../assets/logo/logo.svg';
 import { FaSistrix, FaRegClipboard } from 'react-icons/fa';
 
 const Navigation = () => {
+    // nav links
     const navLink = <>
         <li className='text-xl font-semibold text-[#444444]'><Link>Home</Link></li>
         <li className='text-xl font-semibold text-[#444444]'><Link>About</Link></li>
@@ -14,6 +15,7 @@ const Navigation = () => {
         <div className='lg:w-9/12 mx-auto my-10'>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
+                    {/* small device dropdown navlinks */}
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -22,15 +24,18 @@ const Navigation = () => {
                             {navLink}
                         </ul>
                     </div>
+                    {/* website logo */}
                     <Link to={`/`} className="btn w-28">
                         <img src={logo} alt="website logo" />
                     </Link>
                 </div>
+                {/* big screen navlinks */}
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         {navLink}
                     </ul>
                 </div>
+                {/* buttons */}
                 <div className="navbar-end items-center space-x-5">
                     <FaRegClipboard className='text-2xl text-[#444444]' />
                     <FaSistrix className='text-2xl text-[#444444]' />
