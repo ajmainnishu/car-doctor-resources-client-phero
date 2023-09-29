58ae11cd98d9c224759bcaa495144cfc50afadbf
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-29 11:59:58 +0600

    feat: Services component added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ import Customer from "../Customer/Customer";
 import Features from "../Features/Features";
 import Info from "../Info/Info";
 import Products from "../Products/Products";
+import Services from "../Services/Services";
 import Team from "../Team/Team";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
         <div className="space-y-32">
             <Banner></Banner>
             <AboutUs></AboutUs>
+            <Services></Services>
             <Info></Info>
             <Products></Products>
             <Team></Team>
